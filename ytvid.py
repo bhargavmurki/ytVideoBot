53152fa7771acd523229537ed535961f8d004d8d
bhargavmurki <bhargavmurki@gmail.com>
2023-04-24 01:04:36 -0700

    bug fixes

--- ytvid.py
+++ ytvid.py
@@ -1,8 +1,6 @@
 import logging
 
 from pytube import YouTube
-import requests
-import json
 import boto3
 from botocore.exceptions import ClientError
 
@@ -13,7 +11,6 @@ def upload_file(title):
     try:
         # pattern = re.compile(r"[A-Za-z0-9]+(-[A-Za-z0-9]+)*")
         # title = pattern.search(title.replace(' ', '-')).string
-        print("1234567", title + '.mp4')
 
         s3_client.upload_file(title + '.mp4', "yt-vid-bucket", title + '.mp4')
     except ClientError as e:
@@ -23,11 +20,11 @@ def upload_file(title):
     print("Finished uploading file to S3")
     return True
 
-
-n = int(input("Number of videos to download: "))
+n = int(input("Enter the number of youtube videos to download:   "))
 
 links = []
 
+print("\nEnter all the links one per line:")
 
 for i in range(0, n):
     temp = input()
@@ -36,14 +33,16 @@ for i in range(0, n):
 for i in range(0, n):
     link = links[i]
     yt = YouTube(link)
-    print("\nDetails of the Video", i + 1, "\n")
+    print("\nDetails for Video", i + 1, "\n")
     print("Title: ", yt.title)
+    print("Length: ", yt.length, "seconds")
 
     stream = str(yt.streams.filter(progressive=True))
     stream = stream[1:]
     stream = stream[:-1]
     streamlist = stream.split(", ")
-    print("\nAvailable streams: \n")
+    print("\nAll available options for downloads:\n")
+
     for i in range(0, len(streamlist)):
         st = streamlist[i].split(" ")
         print(i + 1, ") ", st[1], " and ", st[3], sep='')
@@ -55,7 +54,6 @@ for i in range(0, n):
     ys = yt.streams.get_by_itag(tag)
     print("\nDownloading...")
     ys.download('./', filename=yt.title + '.mp4')
-    print("\nDownload completed!!")
-    print()
+    print("\nDownload done")
 
 upload_file(yt.title)
